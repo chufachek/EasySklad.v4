d3b53ad804c716a3676620ef2c48b090784c4c7a
chufachek <153504187+chufachek@users.noreply.github.com>
2026-01-29 22:43:16 +0500

    Update database configuration credentials

--- app/config/db.php
+++ app/config/db.php
@@ -7,9 +7,9 @@ function db()
     }
 
     $host = getenv('DB_HOST') ?: 'localhost';
-    $name = getenv('DB_NAME') ?: 'easysklad';
-    $user = getenv('DB_USER') ?: 'root';
-    $pass = getenv('DB_PASS') ?: '';
+    $name = getenv('DB_NAME') ?: 'nikolxz1_3';
+    $user = getenv('DB_USER') ?: 'nikolxz1_3';
+    $pass = getenv('DB_PASS') ?: 'Qwerty228;';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host={$host};dbname={$name};charset={$charset}";
